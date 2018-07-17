3c696a7b5503c5ebb2a88ec5528555fe02e694c3
maochuang <limaochuang@guazi.com>
2018-07-17 14:59:06 +0800

    project init

---
+++ src/main/java/com/dm/MutidatasourceApplication.java
@@ -0,0 +1,12 @@
+package com.dm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutidatasourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MutidatasourceApplication.class, args);
+	}
+}
